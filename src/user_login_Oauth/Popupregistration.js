ddd24a84a7c86191251b69acbafa35902dd22bd7
msaoud1337 <aminox1998@outlook.fr>
2022-10-02 17:21:01 +0200

    modal styled

--- src/user_login_Oauth/Popupregistration.js
+++ src/user_login_Oauth/Popupregistration.js
@@ -1,14 +1,19 @@
-import React from "react";
+import React,{useState} from "react";
 import "../styles/modal.css";
 import RegistrationCom from "./RegistrationCom";
 
 
 export default function Popupregistration({ show, onClose }) {
+    const [stayLogin, setStayLogin] = useState(false)
     console.log(show)
     show ? document.body.style.overflow = "hidden" : document.body.style.overflow = "visible"
     return (
         show ? <div className="modal" onClick={onClose}>
-            <RegistrationCom onClose={onClose} />
+            <RegistrationCom
+                onClose={onClose}
+                stayLogin={stayLogin}
+                click={() => setStayLogin(!stayLogin)}
+            />
         </div>
         : null
     )
--- src/user_login_Oauth/RegistrationCom.js
+++ src/user_login_Oauth/RegistrationCom.js
@@ -1,17 +1,42 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faXmark} from "@fortawesome/free-solid-svg-icons"
+import { faXmark,faSquare,faSquareCheck } from "@fortawesome/free-solid-svg-icons"
+import {FcGoogle} from "react-icons/fc"
 
-export default function RegistrationCom({onClose}) {
+export default function RegistrationCom({ onClose,stayLogin, setstayLogin, click }) {
     return (
         <div className="modil" onClick={e => e.stopPropagation()}>
             <div className="modil_close">
+                <div className="icone_modal"></div>
                 <FontAwesomeIcon
                     icon={faXmark}
                     onClick={onClose}
-                    className="Log_modal" />
+                    className="Log_modal"
+                />
             </div>    
-                <div className="icone_modal"></div>
-                <h1 className="Log_modal">Log In</h1>
+            <h1 className="Login_in_title">Log In</h1>
+            <div className="user_information">
+                <div className="Email_Address">
+                    <div>Email address</div>
+                    <input type="text" className="inputs"/>
+                </div>
+                <div className="Email_Address">
+                    <div>Password</div>
+                    <input type="text" className="inputs"/>
+                </div>
+                <div className="Forgot_password_bar">
+                    <div className="icone_div">
+                        {stayLogin
+                            ? <FontAwesomeIcon icon={faSquareCheck} onClick={click} />
+                            : <FontAwesomeIcon icon={faSquare} onClick={click}/>}
+                        <div onClick={click} style={{cursor: "Pointer"}}>Remembre me</div>
+                    </div>
+                    <div className="Forgot_password">Forgot password?</div>
+                </div>
+                <button className="Login_button_modal">Log in</button>
+                <div className="Sign_up"> <p>Dont have an account ? </p><p className="Sign_up_1">Sign up</p></div>
+                <div>or</div>
+                <FcGoogle className="Google" />
+            </div>
         </div>
     )
 }
