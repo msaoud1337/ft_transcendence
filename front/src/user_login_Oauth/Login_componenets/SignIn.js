51f3f3209b7fd94723a856125d55726f57026c0e
msaoud1337 <aminox1998@outlook.fr>
2022-10-27 18:46:06 +0200

    feature : Sign_up apdated > request send to back-end

--- front/src/user_login_Oauth/Login_componenets/SignIn.js
+++ front/src/user_login_Oauth/Login_componenets/SignIn.js
@@ -1,8 +1,32 @@
 import Login_user from "../Login_user";
 import "../../styles/Sign.css"
+import { useState } from "react";
 
 export default function SignIn({profile, setProfile, sign}){
     console.log(profile)
+
+    const Inputvalue = {
+        name : "", 
+        password : ""
+    }
+
+    const Login_request = () => {
+        const data_user = {
+            user_name: Inputvalue.name,
+            display_name: Inputvalue.name,
+            password : Inputvalue.password,
+        }
+        console.log(data_user)
+    }
+
+    const changeData_name = (e) => {
+        Inputvalue.name = e.target.value
+    }
+
+    const changeData_password = (e) => {
+        Inputvalue.password = e.target.value
+    }
+
     return (
         <div className="Sign_up_container">
             <div className="Sign_up_pong">Sign In to Pong</div> 
@@ -13,8 +37,8 @@ export default function SignIn({profile, setProfile, sign}){
                 <span>or</span>
             </div>
             <div className="input_containers">
-                <input placeholder="Username / Email" className="User_name_Input"/>
-                <input placeholder="password" className="password_Input"/>
+                <input placeholder="Username / Email" className="User_name_Input" onChange={changeData_name}/>
+                <input type="password" placeholder="password" className="password_Input" onChange={changeData_password}/>
                 <div className="Contunie_container">
                     <button className="Contunie_" onClick={Login_request}>Continue</button>
                 </div>
--- front/src/user_login_Oauth/Login_componenets/SignUp.js
+++ front/src/user_login_Oauth/Login_componenets/SignUp.js
@@ -1,6 +1,51 @@
 import Login_user from "../Login_user";
+import axios from "axios"
+
 
 export default function SignUp({profile, setProfile, sign}) {
+
+    const signUpValue = {
+        name : "",
+        password : "",
+        confirmPassword : "",
+    }
+
+    const userName = (e) => {
+        signUpValue.name = e.target.value
+    }
+
+    const password = (e) => {
+        signUpValue.password = e.target.value
+    }
+
+    const confirmPassword = (e) => {
+        signUpValue.confirmPassword = e.target.value
+    }
+
+    const confirmData = () => {
+        const data = {
+            "user_name": "amine",
+            "display_name": "samine",
+            "email": "samine@1337.com",
+        }
+
+        console.log(data)
+        axios.get("http://localhost:3001/api/users/me", {
+            headers : {
+                "Authorization" : `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MzYsInVzZXJfbmFtZSI6ImFtaW5lIiwiZW1haWwiOiJzYW1pbmVAMTMzNy5jb20iLCJpYXQiOjE2NjY0ODMwODgsImV4cCI6MTY2OTA3NTA4OH0.YnzqK53WhlztTUp7poyuWjWbItGu0g9-lHLfKl_NhXw`
+            }
+        })
+        .then(response => {
+            console.log(response)
+            setProfile(response.data)
+            console.log("data changed in the profile")
+            localStorage.setItem("login_data", JSON.stringify(response.data))
+        })
+        .catch(err) {
+            console.log(err)
+        }
+    }
+
     return (
         <div className="Sign_up_container">
             <div className="Sign_up_pong">Sign Up to Pong</div>
@@ -11,11 +56,11 @@ export default function SignUp({profile, setProfile, sign}) {
                 <span>or</span>
             </div>
             <div className="input_containers">
-                <input placeholder="Username / Email"/>
-                <input placeholder="password"/>
-                <input placeholder="Confirm your password"/>
+                <input placeholder="Username / Email" onChange={userName}/>
+                <input type="password" placeholder="password" onChange={password}/>
+                <input type="password" placeholder="Confirm your password" onChange={confirmPassword}/>
                 <div className="Contunie_container">
-                    <button className="Contunie_">Continue</button>
+                    <button className="Contunie_" onClick={confirmData}>Continue</button>
                 </div>
                 <div className="Join">Already membre ?<span onClick={sign}>Sign in</span></div>
             </div>
