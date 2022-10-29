af24c4e98a888387e112faeff4acd2e137d05965
msaoud1337 <aminox1998@outlook.fr>
2022-10-29 20:08:25 +0200

    feature : user linked with backend

--- front/src/user_login_Oauth/Login_componenets/SignIn.js
+++ front/src/user_login_Oauth/Login_componenets/SignIn.js
@@ -1,30 +1,42 @@
 import Login_user from "../Login_user";
 import "../../styles/Sign.css"
 import { useState } from "react";
+import axios from "axios"
 
-export default function SignIn({profile, setProfile, sign}){
-    console.log(profile)
+export default function SignIn({profile, setProfile, sign, signup_succes, setsignup_succes}){
 
-    const Inputvalue = {
-        name : "", 
-        password : "",
-    }
 
     const Login_request = () => {
-        const data_user = {
-            user_name: Inputvalue.name,
-            display_name: Inputvalue.name,
-            password : Inputvalue.password,
+
+        const user_name = document.querySelector(".User_name_Input")
+        const password = document.querySelector("password_Input")
+
+        axios.post("http://localhost:3001/api/auth/login",
+            {
+                user_name : user_name.value,
+                password : password,
+            }
+        )
+        .then(res => { 
+            axios.get("http://localhost:3001/api/users/me", {
+                headers : {
+                    "Authorization" : `Bearer ${res.data}`
+                }
+            })
+            .then(response => {
+                setProfile(response.data)
+                localStorage.setItem("login_data", JSON.stringify(response.data))
+            })
         }
-        console.log(data_user)
+        )
+        .catch(err =>{
+            console.log(err)
+        })
     }
 
-    const changeData_name = (e) => {
-        Inputvalue.name = e.target.value
-    }
-
-    const changeData_password = (e) => {
-        Inputvalue.password = e.target.value
+    const click = () => {
+        setsignup_succes(false)
+        sign()
     }
 
     return (
@@ -35,16 +47,17 @@ export default function SignIn({profile, setProfile, sign}){
             </div>
              <div className="form-seperator">
                 <span>or</span>
+                {signup_succes && <div className="signup_succes">SignUp succes !!</div>}
             </div>
             <div className="input_containers">
-                <input placeholder="Username / Email" className="User_name_Input" onChange={changeData_name}/>
-                <input type="password" placeholder="password" className="password_Input" onChange={changeData_password}/>
+                <input placeholder="Username / Email" className="User_name_Input"/>
+                <input type="password" placeholder="password" className="password_Input"/>
                 <div className="Contunie_container">
                     <button className="Contunie_" onClick={Login_request}>Continue</button>
                 </div>
             </div>
             <hr className="line"></hr>
-            <div className="Join">Not a membre Yet ? <span onClick={sign}>join</span></div>
+            <div className="Join">Not a membre Yet ? <span onClick={click}>join</span></div>
         </div>
     )
 }
